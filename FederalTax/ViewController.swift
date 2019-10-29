fd6e59c2a51d842be5ab361e8584dc05c4c87075
Russell Gordon <rgordon@lcs.on.ca>
2019-10-28 20:38:29 -0400

    Checks that a name has been provided.

--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var givenName: UITextField!
+    @IBOutlet weak var givenGrossIncome: UITextField!
+    @IBOutlet weak var outputTaxOwingInDollars: UILabel!
+    @IBOutlet weak var outputEffectiveTaxRate: UILabel!
+    
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Calculate the tax owing
+    @IBAction func calculateTax(_ sender: Any) {
+        
+        // Clear the output boxes from last run
+        outputTaxOwingInDollars.text = ""
+        outputEffectiveTaxRate.text = ""
+        
+        // First, get the given name
+        guard let name = givenName.text else {
+            outputTaxOwingInDollars.text = "Please enter a name."
+            return
+        }
+        
+        if name == "" {
+            outputTaxOwingInDollars.text = "Please enter a name."
+            return
+        }
+        
+    }
+    
 }
 
