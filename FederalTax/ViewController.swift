2e3fb44179da5866d966ddd6f9c007cf976a21ca
Russell Gordon <rgordon@lcs.on.ca>
2019-10-28 20:24:41 -0400

    Initial Commit

---
+++ FederalTax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FederalTax
+//
+//  Created by Russell Gordon on 2019-10-28.
+//  Copyright © 2019 Russell Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
