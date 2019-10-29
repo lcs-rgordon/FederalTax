06abb89c4deb5a90e85f90b51cd8e4b3c8898ed8
Russell Gordon <rgordon@lcs.on.ca>
2019-10-28 21:20:15 -0400

    Got main logic done.

--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -55,7 +55,51 @@ class ViewController: UIViewController {
             return
         }
         
-
+        // Total taxes owed
+        var taxOwing = 0.0
+        
+        // Dollars left to tax
+        var dollarsLeftToTax = givenGrossIncomeAsDouble
+        
+        // Determine taxes owed
+        switch givenGrossIncomeAsDouble {
+        case 210371...:
+            let dollarsToTaxAtThisLevel = dollarsLeftToTax - 210371
+            dollarsLeftToTax -= dollarsToTaxAtThisLevel
+            taxOwing += dollarsToTaxAtThisLevel * 0.33
+            fallthrough
+        case 147667...210371:
+            let dollarsToTaxAtThisLevel = dollarsLeftToTax - 147667
+            dollarsLeftToTax -= dollarsToTaxAtThisLevel
+            taxOwing += dollarsToTaxAtThisLevel * 0.29
+            fallthrough
+        case 95259...147667:
+            let dollarsToTaxAtThisLevel = dollarsLeftToTax - 95259
+            dollarsLeftToTax -= dollarsToTaxAtThisLevel
+            taxOwing += dollarsToTaxAtThisLevel * 0.26
+            fallthrough
+        case 47630...95259:
+            let dollarsToTaxAtThisLevel = dollarsLeftToTax - 47630
+            dollarsLeftToTax -= dollarsToTaxAtThisLevel
+            taxOwing += dollarsToTaxAtThisLevel * 0.205
+            fallthrough
+        case 0...47630:
+            let dollarsToTaxAtThisLevel = dollarsLeftToTax
+            dollarsLeftToTax -= dollarsToTaxAtThisLevel
+            taxOwing += dollarsToTaxAtThisLevel * 0.15
+        default:
+            outputTaxOwingInDollars.text = "Error processing dollar amount given."
+        }
+        
+        // Report taxes owing
+        let taxOwingFormatted = String(format: "$%.2f", taxOwing)
+        outputTaxOwingInDollars.text = "\(name) your federal tax owing is \(taxOwingFormatted)"
+        
+        // Report effective tax rate
+        let effectiveTaxRate = taxOwing / givenGrossIncomeAsDouble * 100
+        let effectiveTaxRateFormatted = String(format: "%.1f", effectiveTaxRate)
+        outputEffectiveTaxRate.text = "Effective tax rate is: \(effectiveTaxRateFormatted)%"
+        
         
     }
     
